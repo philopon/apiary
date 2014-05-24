172441b1eb7fb007cd5d701aa1e4bebe4e01efc2
philopon <philopon.dependence@gmail.com>
2014-05-24 17:36:22 +0900

    update documentation, skelton.

--- apiary-logger/src/Web/Apiary/Logger.hs
+++ apiary-logger/src/Web/Apiary/Logger.hs
@@ -1,8 +1,16 @@
 module Web.Apiary.Logger (
-    LogDest(..), LogConfig(..)
-    , HasLogger, withLogger, logging
+    -- * configuration
+    LogDest(..), LogConfig(..), HasLogger
+    -- * initialize
+    , withLogger
+    -- * action
+    , logging
+    -- * wrapper
     , GivenLoggerT(..)
+
+    -- * reexports
+    , module Data.Default.Class
     ) where
 
 import Web.Apiary.Logger.Internal
-
+import Data.Default.Class
