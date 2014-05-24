42464a615db3accf95fc1e6f5e95e18417308208
philopon <philopon.dependence@gmail.com>
2014-05-24 17:18:31 +0900

    persistent: ImplicitParams -> Given. simplify.

--- apiary-persistent/src/Web/Apiary/Database/Persist.hs
+++ apiary-persistent/src/Web/Apiary/Database/Persist.hs
@@ -5,63 +5,35 @@
 {-# LANGUAGE KindSignatures #-}
 
 module Web.Apiary.Database.Persist (
-    -- * configuration
-      ApiaryPersistConfig
-    , defaultApiaryPersistConfig
-
     -- * runner
-    , withWithSqlPool, withWithSqlPool'
+      withWithSqlPool
 
     -- * execute sql
-    , runSql, runSql'
+    , runSql
 
     -- * types
-    , HasPersist, LogRunner
+    , HasPersist
 
     -- * reexport
     , module Database.Persist.Sql
     ) where
 
 import Database.Persist.Sql
-import Control.Monad.Logger
-import Control.Monad.Trans
 import Control.Monad.Trans.Resource
+import Data.Reflection
 
-type HasPersist l =
-    ?webApiaryDatabasePersistState :: ApiaryPersistState l
+type HasPersist = Given ApiaryPersistState
 
-type LogRunner l = forall a. l (ResourceT IO) a -> ResourceT IO a
-
-newtype ApiaryPersistConfig l = ApiaryPersistConfig
-    { runLogger  :: LogRunner l
+newtype ApiaryPersistState = ApiaryPersistState
+    { getPool    :: ConnectionPool
     }
 
-defaultApiaryPersistConfig :: ApiaryPersistConfig NoLoggingT
-defaultApiaryPersistConfig = ApiaryPersistConfig runNoLoggingT
-
-data ApiaryPersistState l = ApiaryPersistState
-    { runLogger' :: LogRunner l
-    , getPool    :: ConnectionPool
-    }
-
-runSql :: (MonadBaseControl IO (l (ResourceT IO)), MonadIO m, HasPersist l) => SqlPersistT (l (ResourceT IO)) a -> m a
-runSql = runSql' (runLogger' ?webApiaryDatabasePersistState)
-
-runSql' :: (MonadBaseControl IO logger, MonadIO m, HasPersist logger') => (logger a -> ResourceT IO b) -> SqlPersistT logger a -> m b
-runSql' run a =
-    liftIO .
-    runResourceT .
-    run $
-    runSqlPool a (getPool ?webApiaryDatabasePersistState)
+runSql :: (MonadBaseControl IO m, HasPersist) => SqlPersistT (ResourceT m) a -> m a
+runSql a =
+    runResourceT $
+    runSqlPool a (getPool given)
 
 withWithSqlPool :: (forall a. (ConnectionPool -> m a) -> m a)
-                -> (HasPersist NoLoggingT => m b) -> m b
-withWithSqlPool = withWithSqlPool' defaultApiaryPersistConfig
-
-withWithSqlPool' :: ApiaryPersistConfig logger
-                 -> (forall a. (ConnectionPool -> m a) -> m a)
-                 -> (HasPersist logger => m b) -> m b
-withWithSqlPool' conf with m = with $ \pool -> do
-    let ?webApiaryDatabasePersistState = ApiaryPersistState (runLogger conf) pool
-    m
-
+                -> (HasPersist => m b) -> m b
+withWithSqlPool with m = with $ \pool -> 
+    give (ApiaryPersistState pool) m
