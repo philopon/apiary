b61402f2734489e1348234bfb1693d6c7128f9bb
philopon <philopon.dependence@gmail.com>
2014-05-24 17:20:31 +0900

    update examples.

--- examples/persistent.hs
+++ examples/persistent.hs
@@ -11,7 +11,9 @@ import Web.Apiary.Database.Persist
 import Network.Wai.Handler.Warp
 import Database.Persist.Sqlite
 import Database.Persist.TH
+import Web.Apiary.Logger
 import qualified Data.ByteString.Lazy.Char8 as L
+import Control.Monad.Logger
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Number 
@@ -20,21 +22,37 @@ Number
 |]
 
 main :: IO ()
-main = withWithSqlPool (withSqlitePool ":memory:" 10) $ do
-    runSql $ runMigration migrateAll
-    run 3000 . runApiary def $ do
-        root . action $ do
-            l <- runSql $ selectList ([] :: [Filter Number]) []
-            lbs $ L.pack (show l)
+-- give logging setting.
+main = withLogger def $
 
-        [capture|/:Int|] $ do
-            stdMethod GET . action $ \i -> do
-                c <- runSql $ count [NumberNumber ==. i]
-                lbs $ L.pack (show c)
+    -- give sql setting.
+    withWithSqlPool (withSqlitePool ":memory:" 10) $ do
 
-            stdMethod POST . action $ \i -> do
-                _ <- runSql $ insert (Number i)
-                return ()
+        -- logging by Given logging setting from 'withLogger'. 
+        -- so logging stderr(buffered).
+        -- you can change this behaviour by first argument of withLogger.
+        runGivenLoggerT . runSql' $ runMigration migrateAll
 
-            stdMethod DELETE . action $ \i -> do
-                runSql $ deleteWhere [NumberNumber ==. i]
+        run 3000 . runApiary def $ do
+            root . action $ do
+
+                -- logging.
+                logging "root accessed.\n"
+
+                l <- runSql' $ selectList ([] :: [Filter Number]) []
+                lbs $ L.pack (show l)
+
+            [capture|/:Int|] $ do
+                stdMethod GET . action $ \i -> do
+
+                    -- if you want to do local logging action.
+                    -- logging stdout immediately.
+                    c <- runStdoutLoggingT . runSql' $ count [NumberNumber ==. i]
+                    lbs $ L.pack (show c)
+
+                stdMethod POST . action $ \i -> do
+                    _ <- runSql' $ insert (Number i)
+                    return ()
+
+                stdMethod DELETE . action $ \i -> do
+                    runSql' $ deleteWhere [NumberNumber ==. i]
