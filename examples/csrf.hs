c51c27e97a69722dfe6e5d1c83d921532baf5007
philopon <philopon.dependence@gmail.com>
2014-05-30 01:32:08 +0900

    add csrf example.

---
+++ examples/csrf.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+import Web.Apiary
+import Web.Apiary.ClientSession
+import Network.Wai.Handler.Warp
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Char8 as S
+
+page :: Monad m => S.ByteString -> ActionT m ()
+page tok = do
+    contentType "text/html"
+    lbs . L.unlines $ 
+        [ "<form method=\"POST\" action=\"/\">"
+        , "<input type=\"text\" name=\"str\"/>"
+        , L.concat ["<input type=\"hidden\" name=\"_token\" value=\"", L.fromStrict tok, "\">"]
+        , "<button type=\"submit\">submit</button>"
+        , "</form>"
+        ]
+
+
+main :: IO ()
+main = withSession def { secure = False } $ run 3000 . runApiary def $ do
+    root $ do  
+        stdMethod GET . action $ csrfToken >>= page
+
+        stdMethod POST . ("str" =: pInt) . checkToken . action $ \i -> do
+            lbs $ L.pack (show i)
+
+    [capture|/:S.ByteString|] . action $ page
