9375c53fbbdfeeb631f01293fa334a5d57175c06
philopon <philopon.dependence@gmail.com>
2014-06-28 01:29:40 +0900

    fix release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd `dirname $0`
+dir=$(cd `dirname $0`; pwd)
 password=`cat password`
 
 for pkg in . `cat submodules`; do
   echo $pkg
-  cd $pkg
+  cd $dir/$pkg
   cabal clean
   file=`cabal sdist | awk '{print $4}'`
   cabal upload -u HirotomoMoriwaki -p $password $file
