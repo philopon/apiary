1860741b005502897036f33c4cba32ca2a443be9
philopon <philopon.dependence@gmail.com>
2014-07-24 17:46:59 +0900

    update purescript example.

--- examples/pureScript.hs
+++ examples/pureScript.hs
@@ -8,19 +8,31 @@ import Network.Wai.Handler.Warp
 import Language.Haskell.TH
 import System.Directory
 import System.FilePath
+import qualified Data.ByteString.Lazy.Char8 as L
 
 -- please run
--- $ bower install purescript-foldable-traversable purescript-arrays
+-- $ bower install philopon/purescript-xhr
 -- in examples directory.
 
 main :: IO ()
 main = do
     setCurrentDirectory $(location >>= stringE . takeDirectory . loc_filename)
+
     withPureScript def {development = True} $ run 3000 . runApiary def $ do
         method GET $ do
             root . action $ do
                 contentType "text/html"
                 file "pureScript.html" Nothing
 
-            [capture|/test.js|] . method GET . action $
+            [capture|/test.js|] . action $
                 pureScript "pureScript.purs"
+
+        [capture|/api/:Int|] .
+            ("test" ?? "b(default: 0)" =?: pInt) . 
+            document "a + b" . action $ \i mbj -> do
+                let j = maybe 0 id mbj
+                contentType "application/json"
+                lbs . L.pack $ "{ \"test\": " ++ show (i + j) ++ "}"
+
+        [capture|/api/documentation|] . method GET . action $
+            defaultDocumentationAction def
--- examples/pureScript.purs
+++ examples/pureScript.purs
@@ -1,5 +1,10 @@
 module Main where
 
+import Control.Monad.Eff
+import Network.XHR
 import Debug.Trace
 
-main = trace "test main"
+main :: Eff (trace :: Trace, xhr :: XHR) Unit
+main = do
+    let cbs = defaultConfig { onLoadEnd = \x -> print (responseText x) }
+    post cbs "/api/12" {test: 24}
