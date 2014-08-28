598015182a75dc4b323e15043b63c493cbf17a6e
philopon <philopon.dependence@gmail.com>
2014-08-28 14:44:19 +0900

    avoid authenticate build failure(because of conduit-1.2 released).

--- .travis-install.sh
+++ .travis-install.sh
@@ -5,6 +5,10 @@ for p in `cat submodules`; do
   path=("${path[@]}" "./$p")
 done
 
+[ -e restriction ] && for p in `cat restriction`; do
+  path=("${path[@]}" "$p")
+done
+
 cabal --version
 cabal sandbox init
 cabal install --force-reinstalls --only-dependencies --enable-tests "${path[@]}"
