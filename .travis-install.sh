4cc7cdb307ca0e3aa4342f615a43acef4e509f39
philopon <philopon.dependence@gmail.com>
2014-08-28 15:01:05 +0900

    fix travis-install.

--- .travis-install.sh
+++ .travis-install.sh
@@ -5,10 +5,13 @@ for p in `cat submodules`; do
   path=("${path[@]}" "./$p")
 done
 
-[ -e restriction ] && for p in `cat restriction`; do
-  path=("${path[@]}" "$p")
-done
-
 cabal --version
 cabal sandbox init
-cabal install --force-reinstalls --only-dependencies --enable-tests "${path[@]}"
+
+if [ -e restriction ]; then
+  cabal install --force-reinstalls --only-dependencies --enable-tests "${path[@]}" `cat restriction`
+  cabal install --force-reinstalls `cat restriction`
+else
+  cabal install --force-reinstalls --only-dependencies --enable-tests "${path[@]}"
+  cabal install --force-reinstalls 
+fi
