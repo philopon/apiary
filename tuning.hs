9e4e2b32068184d9c5707980ba890526051d74b8
philopon <philopon.dependence@gmail.com>
2014-09-07 01:59:47 +0900

    Revert "add tuning program."
    This reverts commit 0e4b676821d155bb463e1cb10900b5744f225626.

--- tuning.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Web.Apiary
-import Network.Wai.Handler.Warp
-
-main :: IO ()
-main = server (run 3000) . runApiary def $ do
-    method GET . action $ do
-        bytes "test"
