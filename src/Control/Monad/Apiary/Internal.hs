2f45404a8ff4ecbdb327284b80962f750ed5f17c
philopon <philopon.dependence@gmail.com>
2015-01-27 05:43:11 +0900

    use new router in ApiaryT

--- src/Control/Monad/Apiary/Internal.hs
+++ src/Control/Monad/Apiary/Internal.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ImpredicativeTypes #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE CPP #-}
@@ -27,14 +29,12 @@ module Control.Monad.Apiary.Internal
 
     -- internal
     , focus, focus'
-    , PathElem(..)
     ) where
-
 
 import qualified Network.Wai as Wai
 
 import Control.Applicative(Applicative(..), (<$>))
-import Control.Monad(liftM, MonadPlus(..))
+import Control.Monad(liftM)
 import Control.Monad.Trans(MonadIO(liftIO), MonadTrans(lift))
 import Control.Monad.Trans.Control
     ( MonadTransControl(..), MonadBaseControl(..)
@@ -43,87 +43,36 @@ import Control.Monad.Trans.Control
 import Control.Monad.Base(MonadBase(..))
 import Control.Monad.Apiary.Action.Internal
     (ActionT, ApiaryConfig, getRequest
-    , modifyState, actionFetches, Extensions(NoExtension)
+    , Extensions(NoExtension)
     , execActionT, hoistActionT, applyDict, rootPattern
     )
 
 import qualified Data.Apiary.Dict as D
+import qualified Data.Apiary.Router as R
 import Data.Apiary.Method(Method, renderMethod)
 import Data.Apiary.Extension ( Has, MonadExts(..), getExt, noExtension )
 import Data.Apiary.Extension.Internal(Initializer(..), allMiddleware, allMiddleware')
 import Data.Apiary.Document.Internal(Doc(..), docsToDocuments)
 import Data.Monoid(Monoid(..), (<>))
 
-import Data.List(foldl')
 import Text.Blaze.Html(Html)
 import qualified Data.Text as T
-import qualified Data.ByteString as S
-import qualified Data.HashMap.Strict as H
 
-data Router exts actM = Router
-    { children   :: H.HashMap T.Text (Router exts actM)
-    , capturing  :: Maybe (Router exts actM)
-    , restMatch  :: Maybe (PathMethod exts actM)
-    , pathMethod :: PathMethod exts actM
-    }
-
-type ActionT' exts actM a = ActionT exts '[] actM a
-
-data PathMethod exts actM = PathMethod
-    { methodMap :: H.HashMap S.ByteString (ActionT' exts actM ())
-    , anyMethod :: Maybe (ActionT' exts actM ())
-    }
-
-emptyRouter :: Router exts actM
-emptyRouter = Router H.empty Nothing Nothing emptyPathMethod
-
-emptyPathMethod :: PathMethod exts actM
-emptyPathMethod = PathMethod H.empty Nothing
-
-insertRouter :: Monad actM => [T.Text] -> Maybe S.ByteString -> [PathElem]
-             -> ActionT' exts actM () -> Router exts actM -> Router exts actM
-insertRouter rootPat mbMethod paths act = loop paths
-  where
-    loop [EndPath] (Router cln cap anp pm) =
-        Router cln cap anp $ insPathMethod pm
-
-    loop [] (Router cln cap anp pm) =
-        Router cln cap (Just . insPathMethod $ maybe emptyPathMethod id anp) pm
-
-    loop (mbp:ps) rtr@(Router cln cap anp pm) = case mbp of
-        FetchPath -> Router cln (Just $ loop ps (maybe emptyRouter id cap)) anp pm
-        Exact p   -> Router (adjust' (loop ps) p cln) cap anp pm
-        EndPath   -> loop ps rtr
-        RestPath  -> Router cln cap (Just . insPathMethod $ maybe emptyPathMethod id anp) pm
-        RootPath  -> let cln' = foldl' (flip $ adjust' (loop [EndPath])) cln rootPat
-                     in loop [EndPath] $ Router cln' cap anp pm
-
-    adjust' f k h = H.adjust f k (H.insertWith (\_ old -> old) k emptyRouter h)
-
-    insPathMethod (PathMethod mm am) = case mbMethod of
-        Nothing -> PathMethod mm (Just $ maybe act (mplus act) am)
-        Just m  -> PathMethod (H.insertWith mplus m act mm) am
-
-data PathElem = Exact {-# UNPACK #-} !T.Text
-              | FetchPath
-              | RootPath
-              | EndPath
-              | RestPath
+type ActionT' exts actM = ActionT exts '[] actM
 
 data ApiaryEnv exts prms actM = ApiaryEnv
-    { envFilter :: ActionT' exts actM (D.Dict prms)
-    , envMethod :: Maybe Method
-    , envPath   :: [PathElem] -> [PathElem]
+    { envMethod :: Maybe Method
+    , envPath   :: R.Path prms (ActionT' exts actM) -> R.Path '[] (ActionT' exts actM)
     , envConfig :: ApiaryConfig
     , envDoc    :: Doc -> Doc
     , envExts   :: Extensions exts
     }
 
 initialEnv :: Monad actM => ApiaryConfig -> Extensions exts -> ApiaryEnv exts '[] actM
-initialEnv conf = ApiaryEnv (return D.empty) Nothing id conf id
+initialEnv conf = ApiaryEnv Nothing id conf id
 
 data ApiaryWriter exts actM = ApiaryWriter
-    { writerRouter :: Router exts actM -> Router exts actM
+    { writerRouter :: R.Router '[] (ActionT' exts actM) -> R.Router '[] (ActionT' exts actM)
     , writerDoc    :: [Doc] -> [Doc]
     , writerMw     :: Wai.Middleware
     }
@@ -145,31 +94,10 @@ apiaryT :: Monad m
         -> ApiaryT exts prms actM m a
 apiaryT f = ApiaryT $ \rdr cont -> f rdr >>= \(a, w) -> cont a w
 
-routerToAction :: Monad actM => Router exts actM -> ActionT' exts actM ()
-routerToAction router = getRequest >>= go
-  where
-    go req = loop id router (Wai.pathInfo req)
-      where
-        method = Wai.requestMethod req
-
-        pmAction nxt (PathMethod mm am) =
-            let a = maybe nxt id am
-            in maybe a (`mplus` a) $ H.lookup method mm
-
-        loop fch (Router _ _ anp pm) [] = do
-            modifyState (\s -> s { actionFetches = fch [] } )
-            pmAction (maybe mzero (pmAction mzero) anp) pm 
-
-        loop fch (Router c mbcp anp _) (p:ps) = case mbcp of
-            Nothing -> cld ana
-            Just cp -> cld $ loop (fch . (p:)) cp ps `mplus` ana
-          where
-            ana = do
-                modifyState (\s -> s {actionFetches = fch $ p:ps} ) 
-                maybe mzero (pmAction mzero) anp
-            cld nxt = case H.lookup p c of
-                Nothing -> nxt
-                Just cd -> loop fch cd ps `mplus` nxt
+routerToAction :: Monad actM => R.Router '[] (ActionT' exts actM) -> ActionT' exts actM ()
+routerToAction router = do
+    req <- getRequest
+    R.execute router (Wai.requestMethod req) (Wai.pathInfo req)
 
 -- | run Apiary monad.
 runApiaryTWith :: (Monad actM, Monad m)
@@ -182,7 +110,7 @@ runApiaryTWith :: (Monad actM, Monad m)
 runApiaryTWith runAct run (Initializer ir) conf m = ir NoExtension $ \exts -> do
     wtr <- unApiaryT m (initialEnv conf exts) (\_ w -> return w)
     let doc = docsToDocuments $ writerDoc wtr []
-        rtr = writerRouter wtr emptyRouter
+        rtr = writerRouter wtr R.empty
         mw  = allMiddleware exts . writerMw wtr
         mw' = allMiddleware' exts
         app = mw $ execActionT conf exts doc (mw' $ hoistActionT runAct $ routerToAction rtr)
@@ -284,31 +212,30 @@ focus :: Monad actM
       => (Doc -> Doc)
       -> ActionT exts prms actM (D.Dict prms')
       -> ApiaryT exts prms' actM m () -> ApiaryT exts prms actM m ()
-focus d g m = focus' d Nothing id g m
+focus doc a = focus' doc Nothing $ R.raw "focus" $ \d t ->
+    applyDict d a >>= \d' -> return (d', t)
 
 focus' :: Monad actM
        => (Doc -> Doc)
        -> Maybe Method
-       -> ([PathElem] -> [PathElem])
-       -> ActionT exts prms actM (D.Dict prms')
+       -> (R.Path prms' (ActionT' exts actM) -> R.Path prms (ActionT' exts actM))
        -> ApiaryT exts prms' actM m () -> ApiaryT exts prms actM m ()
-focus' d meth pth g m = ApiaryT $ \env cont -> unApiaryT m env 
-    { envFilter = envFilter env >>= flip applyDict g
-    , envMethod = maybe (envMethod env) Just meth
-    , envPath   = envPath env . pth
-    , envDoc    = envDoc env  . d
+focus' d meth pth m = ApiaryT $ \ApiaryEnv{..} cont -> unApiaryT m ApiaryEnv
+    { envMethod = maybe envMethod Just meth
+    , envPath   = envPath . pth
+    , envDoc    = envDoc  . d
+    , envConfig = envConfig
+    , envExts   = envExts
     } cont
 
 -- | splice ActionT to ApiaryT.
 action :: Monad actM => ActionT exts prms actM () -> ApiaryT exts prms actM m ()
 action a = do
     env <- getApiaryEnv
+    let meth = renderMethod <$> envMethod env
+        path = envPath env (R.leaf meth $ flip applyDict a)
     addRoute $ ApiaryWriter
-        (insertRouter
-            (rootPattern $ envConfig env)
-            (renderMethod <$> envMethod env)
-            (envPath env [])
-            (envFilter env >>= flip applyDict a))
+        (R.add path)
         (envDoc env Action:)
         id
 
