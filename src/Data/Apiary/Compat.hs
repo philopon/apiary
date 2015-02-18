142273644e40b65ffc95e15743fb9458aa20c13e
philopon <philopon.dependence@gmail.com>
2015-02-19 02:49:04 +0900

    update compat module

--- src/Data/Apiary/Compat.hs
+++ src/Data/Apiary/Compat.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE UndecidableInstances #-} -- for ghc-7.6
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE FlexibleInstances #-} -- for ghc-7.6
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE KindSignatures #-}
@@ -8,32 +10,51 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE CPP #-}
 
+#ifndef MIN_VERSION_base
+#define MIN_VERSION_base(a,b,c) 1
+#endif
+
 -- | compatibility module for ghc-7.8 & ghc-7.6.
 module Data.Apiary.Compat
     ( -- * type level string literal
       Symbol, KnownSymbol, symbolVal
     , SProxy(..)
 
-    -- * Data.Typeables
-    , module Data.Typeable
-#if !MIN_VERSION_base(4,7,0)
-    , typeRep
     , Proxy(..)
+    , module Data.Typeable
+#if MIN_VERSION_base(4,7,0)
+    , Typeable.typeRep
+#else
+    , typeRep
 #endif
     ) where
 
 import GHC.TypeLits
 import Data.Typeable
-#if !MIN_VERSION_base(4,7,0)
+    ( Typeable, typeOf, cast, gcast
+    , TypeRep, showsTypeRep
+    , TyCon, tyConString, tyConPackage, tyConModule, tyConName
+    , mkTyCon3, mkTyConApp, mkAppTy, mkFunTy
+    , splitTyConApp, funResultTy, typeRepTyCon, typeRepArgs
+    )
+
+#if MIN_VERSION_base(4,7,0)
+import Data.Proxy(Proxy(..))
+import qualified Data.Typeable as Typeable
+#else
 
 typeRep :: forall proxy a. Typeable a => proxy a -> TypeRep
 typeRep _ = typeOf (undefined :: a)
 {-# INLINE typeRep #-}
 
-type KnownSymbol (n :: Symbol) = SingRep n String
+class SingI n => KnownSymbol (n :: Symbol) where
+    symbolSing :: Sing n
+
+instance SingRep n String => KnownSymbol n where
+    symbolSing = sing
 
 symbolVal :: forall n proxy. KnownSymbol n => proxy n -> String
-symbolVal _ = fromSing (sing :: Sing n)
+symbolVal _ = fromSing (symbolSing :: Sing n)
 {-# INLINE symbolVal #-}
 
 data Proxy (a :: k) = Proxy
