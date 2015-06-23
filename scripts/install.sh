ef66a2b1230a67ab8e24b2daa29eea900820d181
philopon <philopon.dependence@gmail.com>
2015-06-23 22:57:20 +0900

    fixed install.sh (new relic sdk URL changed)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash -eu
 
 if [ $# -eq 0 ]; then
-  BASE=http://download.newrelic.com/agent_sdk
-  FILE=`curl $BASE/ | sed -e '/<\/a>/ !d; s/.*<a *href="\(.*\)">.*/\1/g; /tar\.gz/ !d'`
-  curl -O $BASE/$FILE
+  FILE=`curl -L https://dl.dropboxusercontent.com/u/2695969/get-latest-newrelic-sdk.sh | bash`
   tar xvf $FILE
   sudo cp ${FILE%.tar.gz}/include/* /usr/local/include
   sudo cp ${FILE%.tar.gz}/lib/*     /usr/local/lib
