54ac06ed75be372b8ae6b5a6ca09c6278300d598
philopon <philopon.dependence@gmail.com>
2015-08-30 21:05:47 +0900

    container travis

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash -eu
 
-if [ $# -eq 0 ]; then
-  FILE=`curl -L https://dl.dropboxusercontent.com/u/2695969/get-latest-newrelic-sdk.sh | bash`
-  tar xvf $FILE
-  sudo cp ${FILE%.tar.gz}/include/* /usr/local/include
-  sudo cp ${FILE%.tar.gz}/lib/*     /usr/local/lib
-fi
-
-[ "${1-}" == "develop" ] && shift
+FILE=`curl -L https://gist.githubusercontent.com/philopon/b51788ae5c217dc7d269/raw/get-latest-newrelic-agent.sh | bash`
+tar xvf $FILE
+NEWRELIC_BASE=`pwd`/${FILE%.tar.gz}
+export LD_LIBRARY_PATH=$NEWRELIC_BASE/lib
 
 path=("." "./examples")
 for p in `cat submodules`; do
   path=("${path[@]}" "./$p")
 done
 
-cabal --version
-cabal sandbox init
-
-if [ -e constraint ]; then
-  cnst=()
-  while read line; do
-    cnst=("--constraint=$line" "$cnst")
-  done < constraint
-
-  echo cabal install --force-reinstalls --reorder-goals --only-dependencies --enable-tests "${path[@]}" "${cnst[@]}" "$@"
-  cabal install --force-reinstalls --reorder-goals --only-dependencies --enable-tests "${path[@]}" "${cnst[@]}" "$@"
-else
-  echo cabal install --force-reinstalls --reorder-goals --only-dependencies --enable-tests "${path[@]}" "$@"
-  cabal install --force-reinstalls --reorder-goals --only-dependencies --enable-tests "${path[@]}" "$@"
-fi
+cabal update
+cabal install\
+  --force-reinstalls --reorder-goals --only-dependencies --enable-tests\
+  --extra-include-dirs=$NEWRELIC_BASE/include\
+  --extra-lib-dirs=$NEWRELIC_BASE/lib\
+  "${path[@]}"
